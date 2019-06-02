da70b7c23cde5710156be5fd8fb341e43ad46624
Neromaru <near.3dsmax@gmail.com>
2019-06-02 13:23:34 +0300

    added search form

--- django_personal/views.py
+++ django_personal/views.py
@@ -1,11 +1,34 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, View
+from django.shortcuts import render
 
 from .models import Good
+from .forms import SearchForm
 # Create your views here.
 
 
-class IndexView(ListView):
+class IndexView(View):
     model = Good
+    form_class = SearchForm
+    initial = {'key':'value'}
     template_name = 'django_personal/goods_list.html'
     context_object_name = 'goods_list'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, 'django_personal/goods_list.html',
+                      {self.context_object_name: self.model.objects.all(),
+                       'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            text = data['text_field']
+            response = self.model.objects.filter(name__contains=text)
+            return render(request, 'django_personal/goods_list.html',
+                          {self.context_object_name: response,
+                              'form': form})
+        return render(request, 'django_personal/goods_list.html',
+                      {self.context_object_name: self.model.objects.all(),
+                       'form': form})
 
