e75616093a49445d65fe04bec3be4a703ae0e970
neromaru <near.3dsmax@mgail.com>
2019-06-10 04:39:01 +0300

    stable work of celery

--- SoftServePersonal/django_personal/tasks.py
+++ SoftServePersonal/django_personal/tasks.py
@@ -1,8 +1,9 @@
 from celery import shared_task, current_app
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.conf import settings
 from .models import GoodsToNotificateAbout, Good
 from django_celery_beat.models import PeriodicTask, CrontabSchedule
+from django.core import serializers
 
 
 @shared_task
@@ -14,11 +15,11 @@ def delay_send_email():
             'interested_good', 'interested_min_price',
             'interested_max_price', 'user__email'):
         list_of_goods = model_good.objects.filter(
-            name__contains=notification.interested_good,
-            price__range=(notification.interested_min_price,
-                          notification.interested_max_price)
+            name__contains=notification.get('interested_good'),
+            price__range=(notification.get('interested_min_price'),
+                          notification.get('interested_max_price'))
             ).order_by('-price')[:limit]
-        data = dict(goods_list=list_of_goods, email=notification.get(
+        data = dict(goods_list=serializers.serialize('json', list_of_goods), email=notification.get(
             'user__email'))
         return send_email_to_user.delay(data)
 
@@ -30,12 +31,13 @@ def email_notifications_for_users():
 
 @shared_task
 def send_email_to_user(data):
-    send_mail(
+    msg = EmailMessage(
         'List of goods you were interested in',
         f'{data.get("goods_list")}',
-        settings.EMAIL_HOST_USER,
-        data.get('email')
+        to=[data.get('email')]
         )
+    msg.send()
+    return data
 
 
 @shared_task
